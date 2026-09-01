ce154024c57648cb1fe14b760054daf88fae8b71
Patton Gerrels <pgerrels@gmail.com>
2026-09-01 11:36:40 -0500

    Week 2 Lab Assignment

---
+++ CSCI_271_WEEK2_Lab.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // TODO: declare your variable and assign a value to your variable
+    int luckyNumber = 7;
+
+
+    // TODO: print your variable with cout
+    cout << "My lucky number is: " << luckyNumber << endl;
+    return 0; // end of program
+}
